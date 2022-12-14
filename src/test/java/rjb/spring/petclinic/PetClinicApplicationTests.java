397ba32e3d6cf9fb3f74eee3884e56d90453f9c7
robbbowes <up666724@myport.ac.uk>
2022-12-14 19:00:52 +0100

    Initial Project Add

---
+++ src/test/java/rjb/spring/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package rjb.spring.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
